014183188ada4c99b65f3e7c7832ee1136e1d9a5
Marco Cedaro <cedmax@users.noreply.github.com>
2018-06-02 10:06:21 +0100

    added more tests and fixed them

--- src/content/lodash/object/merge/spec.js
+++ src/content/lodash/object/merge/spec.js
@@ -1,30 +1,32 @@
 const expected = {
   simple: { a: [{ b: 2, c: 3 }, { d: 4, e: 5 }] },
+  array: [{ a: 1, b: 1, f: 1 }, { b: 2, c: 5 }, { c: 3, d: 3, e: 3 }, [4, 5, 6, 7]],
   complex: [{
     a: 'testdata',
     b: 555,
     c: {
       ca: 1,
-      cb: [
-        {
-          a: 'test',
-          x: 2,
-        },
-        {
-          b: 56,
-        },
-        {
-          c: 1,
-        },
-      ],
+      cb: [{
+        a: 'test',
+        x: 2,
+      }, {
+        b: 56,
+      }, {
+        c: 1,
+      }, {
+        d: 12,
+      }],
       cc: 1,
     },
+    d: 1,
   }, {
     a: 1,
-    b: 2,
+    b: 'abc',
     c: 'def',
   }, {
     a: 'test',
+  }, {
+    c: 2,
   }],
 }
 
@@ -36,6 +38,10 @@ test('merge simple', () => {
   expect(plain.simple).toEqual(lodash.simple)
 })
 
+test('merge array', () => {
+  expect(lodash.array).toEqual(expected.array)
+  expect(plain.array).toEqual(lodash.array)
+})
 
 test('merge complex', () => {
   expect(lodash.complex).toEqual(expected.complex)
--- src/content/lodash/object/merge/vanilla.js
+++ src/content/lodash/object/merge/vanilla.js
@@ -10,11 +10,10 @@ function merge (...args) {
   const firstArgIsArray = isArray(firstArg)
 
   if (!firstArgIsObject && !firstArgIsArray) {
-    return firstArg
+    return secondArg || firstArg
   }
 
   const result = firstArgIsObject ? {} : []
-
   return args.reduce((result, currentArg) => {
     Object.entries(currentArg).forEach(([key, value]) => {
       if (isObject(value) && isObject(result[key])) {
@@ -53,6 +52,13 @@ const other = {
 exports.simple = merge(object, other)
 // => { 'a': [{ 'b': 2, 'c': 3 }, { 'd': 4, 'e': 5 }] }
 
+const array = [{ a: 1 }, {b: 2}, {c: 3}, [1, 2, 3]]
+const anotherArray = [{ b: 1 }, {c: 2}, {d: 3}, [4, 5, 6, 7]]
+const lastArray = [{ f: 1 }, {c: 5}, {e: 3}, []]
+
+exports.array = merge(array, anotherArray, lastArray)
+// [{"a": 1, "b": 1, "f": 1}, {"b": 2, "c": 5}, {"c": 3, "d": 3, "e": 3}, [4, 5, 6, 7]]
+
 const arrayWithObjects = [{
   a: 'testdata',
   b: 123,
@@ -79,5 +85,22 @@ const otherArray = [{
   c: 'def',
 }]
 
-exports.complex = merge(arrayWithObjects, otherArray)
+const yetAnotherOne = [{
+  c: {
+    cb: [{ a: 'test', x: 2 }, { b: 56 }, { c: 1 }, { d: 12 }],
+  },
+}]
+
+const evenMore = [{
+  d: 1,
+}, {
+  a: 1,
+  b: 'abc',
+}, {
+  a: 'test',
+}, {
+  c: 2,
+}]
+
+exports.complex = merge(arrayWithObjects, otherArray, yetAnotherOne, evenMore)
 // look at the spec file for the output
