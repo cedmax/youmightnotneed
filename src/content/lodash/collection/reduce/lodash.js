fd9dae0dc65332c3d679354704d090cb7b0da656
Marco Cedaro <cedmax@users.noreply.github.com>
2018-10-19 09:31:48 +0100

    enhanced reduce

--- src/content/lodash/collection/reduce/lodash.js
+++ src/content/lodash/collection/reduce/lodash.js
@@ -1,4 +1,10 @@
 import { reduce } from 'lodash'
 
-module.exports = reduce([0, 1, 2, 3], (a, b) => a + b)
-// => 6
+exports.array = reduce([1, 2], (sum, n) => sum + n, 0)
+// => 3
+
+exports.object = reduce({ a: 1, b: 2, c: 1 }, (result, value, key) => {
+  (result[value] || (result[value] = [])).push(key)
+  return result
+}, {})
+// => { '1': ['a', 'c'], '2': ['b'] } (iteration order is not guaranteed)
--- src/content/lodash/collection/reduce/spec.js
+++ src/content/lodash/collection/reduce/spec.js
@@ -1,9 +1,17 @@
-const expected = 6
+const expected = {
+  array: 3,
+  object: { 1: ['a', 'c'], 2: ['b'] },
+}
 
 const lodash = require('./lodash')
 const plain = require('./vanilla')
 
-test('reduce', () => {
-  expect(lodash).toEqual(expected)
-  expect(plain).toEqual(lodash)
+test('array', () => {
+  expect(lodash.array).toEqual(expected.array)
+  expect(plain.array).toEqual(lodash.array)
+})
+
+test('object', () => {
+  expect(lodash.object).toEqual(expected.object)
+  expect(plain.object).toEqual(lodash.object)
 })
