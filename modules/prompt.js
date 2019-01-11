86bffeaa3e57cea8d112360d48fcbe8957757884
Marco Cedaro <cedmax@users.noreply.github.com>
2019-01-11 12:59:33 +0000

    refactoring

--- modules/prompt.js
+++ modules/prompt.js
@@ -1,47 +1,14 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
-const capitalize = require('lodash/capitalize')
-const { join } = require('path')
-
-const listProjects = p =>
-  fs
-    .readdirSync(p)
-    .filter(f => fs.statSync(join(p, f)).isDirectory())
-    .map(dir => capitalize(dir))
+const { getProject, getSection, getSectionInput, getMethod } = require('./questions')
 
 module.exports = async base => {
-  const { project } = await inquirer.prompt({
-    name: 'project',
-    type: 'list',
-    message: 'What project?',
-    choices: listProjects(`${base}`),
-  })
-  let { section } = await inquirer.prompt({
-    name: 'section',
-    type: 'list',
-    message: 'What section?',
-    choices: [...listProjects(`${base}/${project}`), 'I want to add a missing one'],
-  })
-
-  if (section === 'I want to add a missing one') {
-    const { section: inputSection } = await inquirer.prompt({
-      name: 'section',
-      type: 'input',
-      message: 'What section?',
-    })
-    section = inputSection
-  }
-
-  const { method } = await inquirer.prompt({
-    name: 'method',
-    type: 'input',
-    message: 'What method?',
-  })
+  const project = await getProject(base)
+  const section = (await getSection(base, project)) || (await getSectionInput())
+  const method = await getMethod()
 
   return {
     base,
-    project: project.toLowerCase(),
-    section: section.toLowerCase(),
-    method: method.toLowerCase(),
+    project,
+    section,
+    method,
   }
 }
