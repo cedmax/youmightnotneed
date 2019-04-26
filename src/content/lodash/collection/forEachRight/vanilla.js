8d1100784d0dfc03cb814fc2b420c66220bbed7c
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-04-26 12:03:34 -0400

    Create vanilla implementation of _.forEachRight

---
+++ src/content/lodash/collection/forEachRight/vanilla.js
@@ -0,0 +1,12 @@
+function forEachRight (array, iteratee) {
+    array.slice().reverse().forEach((val, index) => {
+        iteratee(val, Math.abs(arr.length - index), arr)
+    })
+}
+
+const array = ['a', 'b', 'c']
+
+module.exports = forEachRight(array, iteratee)
+// => c 2
+// => b 1
+// => a 0
