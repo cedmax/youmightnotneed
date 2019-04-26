54271a5b3f7264d6294c6e683e5672d95401c2d3
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-04-26 12:01:14 -0400

    Create Lodash example for _.forEachRight

---
+++ src/content/lodash/collection/forEachRight/lodash.js
@@ -0,0 +1,12 @@
+import { forEachRight } from 'lodash'
+
+function iteratee (item, index) {
+  console.log(item, index)
+}
+
+const array = ['a', 'b', 'c']
+
+module.exports = forEachRight(array, iteratee)
+// => c 2
+// => b 1
+// => a 0
