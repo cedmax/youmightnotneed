b9a5165ae1a090b1bf8db0afc0f1f5d29d326e7d
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-05-04 15:47:20 -0400

    Fix undefined variable errors

--- src/content/lodash/collection/forEachRight/vanilla.js
+++ src/content/lodash/collection/forEachRight/vanilla.js
@@ -1,12 +1,14 @@
 function forEachRight (array, iteratee) {
   array.slice().reverse().forEach((val, index) => {
-    iteratee(val, Math.abs(arr.length - index), arr)
+    iteratee(val, Math.abs(array.length - index), array)
   })
 }
 
 const array = ['a', 'b', 'c']
 
-module.exports = forEachRight(array, iteratee)
+module.exports = forEachRight(array, (item, index) => {
+  console.log(item, index)
+})
 // => c 2
 // => b 1
 // => a 0
