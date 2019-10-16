ee95cf6612f6efc1f1d1cfef0acef39c8abedaa8
Marco Cedaro <cedmax@users.noreply.github.com>
2019-10-16 20:57:40 +0100

    Fix typo (#47)

--- src/content/lodash/math/meanBy/vanilla.js
+++ src/content/lodash/math/meanBy/vanilla.js
@@ -1,12 +1,12 @@
-const minBy = (arr, iteratee) => {
+const meanBy = (arr, iteratee) => {
   const func = typeof iteratee === 'function' ? iteratee : item => item[iteratee]
   return arr.reduce((acc, item) => acc + func(item), 0) / arr.length
 }
 
 const objects = [{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }]
 
-exports.function = minBy(objects, o => o.n)
+exports.function = meanBy(objects, o => o.n)
 // => 5
 
-exports.shortHand = minBy(objects, 'n')
+exports.shortHand = meanBy(objects, 'n')
 // => 5
