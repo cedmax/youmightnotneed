4bb8d4b1e478d926b101a7a5e42017c7ccf88aa1
Marco Cedaro <cedmax@users.noreply.github.com>
2019-10-24 21:58:35 +0100

    fix config for react static

--- static.config.js
+++ static.config.js
@@ -1,99 +1,46 @@
-import React, { Component } from "react";
-import { ServerStyleSheet } from "styled-components";
-import marked from "marked";
+import React from 'react'
 
-const renderer = new marked.Renderer();
-renderer.link = (href, title, text) => {
-  const isExternal = href.indexOf("http") === 0;
-  return `<a href="${href}"
-    ${isExternal ? 'target="_blank" rel="noopener"' : ""} 
-    ${title ? `title="${title}"` : ""}>${text}</a>`;
-};
-
-const envConfig = {};
+const envConfig = {}
 if (process.env.PULL_REQUEST) {
-  envConfig.siteRoot = (process.env.PULL_REQUEST === "true"
+  envConfig.siteRoot = (process.env.PULL_REQUEST === 'true'
     ? process.env.DEPLOY_PRIME_URL
     : process.env.URL
-  ).replace("http://", "https://");
+  ).replace('http://', 'https://')
 }
 
 export default {
   ...envConfig,
   getRoutes: async () => [
     {
-      path: "/",
-      component: "src/containers/Home",
+      path: '/',
+      template: 'src/containers/Home',
     },
     {
-      path: "/lodash",
-      component: "src/containers/Lodash",
+      path: '/lodash',
+      template: 'src/containers/Lodash',
     },
     {
-      path: "/lodash/missing",
-      component: "src/containers/LodashMissing",
+      path: '/lodash/missing',
+      template: 'src/containers/LodashMissing',
     },
     {
-      path: "/momentjs",
-      component: "src/containers/Moment",
+      path: '/momentjs',
+      template: 'src/containers/Moment',
     },
     {
-      path: "/css",
-      component: "src/containers/Css",
+      path: '/css',
+      template: 'src/containers/Css',
     },
   ],
-  renderToHtml: (render, Comp, meta) => {
-    const sheet = new ServerStyleSheet();
-    const html = render(sheet.collectStyles(<Comp />));
-    meta.styleTags = sheet.getStyleElement();
-    return html;
-  },
-  Document: class CustomHtml extends Component {
-    render() {
-      const { Html, Head, Body, children, renderMeta } = this.props;
-
-      return (
-        <Html>
-          <Head>
-            <meta
-              name="description"
-              content='A collection of "You might not need" resources'
-            />
-            <meta charSet="UTF-8" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            {renderMeta.styleTags}
-          </Head>
-          <Body>{children}</Body>
-        </Html>
-      );
-    }
-  },
-  webpack: [
-    config => {
-      config.module.rules.push({
-        test: /\/content\/(.*).js$/,
-        use: "raw-loader",
-      });
-
-      config.module.rules[0].oneOf.unshift({
-        test: /\.md$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-          {
-            loader: "markdown-loader",
-            options: {
-              renderer,
-            },
-          },
-        ],
-      });
-
-      return config;
-    },
-  ],
-};
+  Document: ({ Html, Head, Body, children }) => (
+    <Html>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="description" content='A collection of "You might not need" resources' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
+  plugins: ['react-static-plugin-styled-components', 'custom-raw-loader'],
+}
