e992efd6e3da9edc828a1560198fca43be868a78
Roman Gusev <rgusev@hwdtech.ru>
2019-12-08 14:45:33 +0600

    Improve _.fromPairs replacement

--- src/content/lodash/array/fromPairs/vanilla.js
+++ src/content/lodash/array/fromPairs/vanilla.js
@@ -1,8 +1,2 @@
-const fromPairs = pairs =>
-  pairs.reduce((cache, pair) => {
-    cache[pair[0]] = pair[1]
-    return cache
-  }, {})
-
-module.exports = fromPairs([['a', 1], ['b', 2]])
+module.exports = Object.fromEntries([['a', 1], ['b', 2]])
 // => { 'a': 1, 'b': 2 }
