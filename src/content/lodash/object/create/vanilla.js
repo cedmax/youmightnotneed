b0d2facb7588b1d57e2b5beda009843defcd499e
Marco Cedaro <cedmax@users.noreply.github.com>
2020-02-25 15:58:23 +0000

    Object#create

---
+++ src/content/lodash/object/create/vanilla.js
@@ -0,0 +1,20 @@
+// https://lodash.com/docs/#create
+function Shape() {
+  this.x = 0
+  this.y = 0
+}
+
+function Circle() {
+  Shape.call(this)
+}
+
+Circle.prototype = Object.assign(Object.create(Shape.prototype), {
+  constructor: Circle,
+})
+
+const circle = new Circle()
+exports.circle = circle instanceof Circle
+// => true
+
+exports.shape = circle instanceof Shape
+// => true
