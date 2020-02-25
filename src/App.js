42e7c34abd279c106d7ffcbdcd6f732449b88262
Marco Cedaro <cedmax@users.noreply.github.com>
2020-02-25 13:41:43 +0000

    removed useless comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Suspense } from 'react'
 import { Root, Routes } from 'react-static'
-// eslint-disable-next-line import/no-unresolved
 import { Router } from '@reach/router'
 import AppStyles, { GlobalStyle } from './helpers/styles'
 import Footer from './components/Footer'
