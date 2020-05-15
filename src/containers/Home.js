8cd142d49e6d695a44a11d7c9751536deef8424e
Marco Cedaro <cedmax@users.noreply.github.com>
2020-05-15 07:27:10 +0100

    removed page per lib in favour of generics with route data

--- src/containers/Home.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import content from '../content/readme.md'
-import Page from '../components/Page'
-
-export default () => <Page title="You Might Not Need *" content={content} />
