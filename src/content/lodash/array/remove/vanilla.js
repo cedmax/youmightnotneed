a183a21025a7a18890bd0631da1e4f172885bf14
Marco Cedaro <cedmax@users.noreply.github.com>
2020-07-09 21:23:59 +0100

    comments

--- src/content/lodash/array/remove/vanilla.js
+++ src/content/lodash/array/remove/vanilla.js
@@ -1,11 +1,11 @@
 const remove = (array, iteratee) => {
   // in order to not mutate the original array until the very end
-  // we want to cache the index to remove while preparing the result
+  // we want to cache the indexes to remove while preparing the result to return
   const toRemove = []
   const result = array.filter((item, i) => iteratee(item) && toRemove.push(i))
 
-  // just before returning we can then remove the items, but making sure it's
-  // reversed, because otherwise the indexes would shift at each removal
+  // just before returning, we can then remove the items, making sure we start
+  // from the higher indexes: otherwise they would shift at each removal
   toRemove.reverse().forEach(i => array.splice(i, 1))
   return result
 }
