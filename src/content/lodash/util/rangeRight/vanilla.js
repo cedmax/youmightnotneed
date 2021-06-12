76bdbc32d1bf1aa8120947da5e24f231eb67381b
Marco Cedaro <cedmax@users.noreply.github.com>
2021-06-12 12:10:30 +0100

    better docs

--- src/content/lodash/util/rangeRight/vanilla.js
+++ src/content/lodash/util/rangeRight/vanilla.js
@@ -1,18 +1,30 @@
 const rangeRight = (start, end, increment) => {
+  // if the end is not defined...
   const isEndDef = typeof end !== 'undefined'
+  // ...the first argument should be the end of the range...
   end = isEndDef ? end : start
+  // ...and 0 should be the start
   start = isEndDef ? start : 0
 
+  // if the increment is not defined, we could need a +1 or -1
+  // depending on whether we are going up or down
   if (typeof increment === 'undefined') {
     increment = Math.sign(end - start)
   }
 
+  // calculating the lenght of the array, which has always to be positive
   const length = Math.abs((end - start) / (increment || 1))
 
+  // In order to return the right result, we need to create a new array
+  // with the calculated length and fill it with the items starting from
+  // the start value + the value of increment.
   const { result } = Array.from({ length }).reduce(
     ({ result, current }) => ({
-      current: current + increment,
+      // prepend the current value to the result array
       result: [current, ...result],
+      // adding the increment to the current item
+      // to be used in the next iteration
+      current: current + increment,
     }),
     { current: start, result: [] }
   )
