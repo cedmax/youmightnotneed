a61ecaff33ea00f4d5a3a7e991ae8b4c223f88ae
Marco Cedaro <cedmax@users.noreply.github.com>
2022-05-24 21:31:16 +0100

    fix aria for navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,30 @@
 import React, { memo } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-export default memo(() => (
-  <nav>
-    <Link href="/">You Might Not Need</Link>
-    <Link href="/lodash">lodash</Link>
-    <Link href="/date-fns">date-fns</Link>
-    <Link href="/css">css</Link>
-  </nav>
-))
+export default memo(() => {
+  const router = useRouter()
+
+  return (
+    <nav>
+      <Link href="/">
+        <a aria-current={router.asPath == '/' ? 'page' : undefined}>
+          You Might Not Need
+        </a>
+      </Link>
+      <Link href="/lodash">
+        <a aria-current={router.asPath == '/lodash' ? 'page' : undefined}>
+          lodash
+        </a>
+      </Link>
+      <Link href="/date-fns">
+        <a aria-current={router.asPath == '/date-fns' ? 'page' : undefined}>
+          date-fns
+        </a>
+      </Link>
+      <Link href="/css">
+        <a aria-current={router.asPath == '/css' ? 'page' : undefined}>css</a>
+      </Link>
+    </nav>
+  )
+})
