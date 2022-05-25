dcccc1dc37421cdf66aa59a011b2dea3b25c39af
Marco Cedaro <cedmax@users.noreply.github.com>
2022-05-25 21:30:19 +0100

    refactoring

--- src/helpers/static-props.js
+++ src/helpers/static-props.js
@@ -9,12 +9,14 @@ const requireMarkdown = requireMarkdownC({
   html: true,
 })
 
+const parseMarkdown = file => requireMarkdown(path.resolve(file))
+
 const fetchContent = prj => {
   const obj = {}
   glob.sync(`./src/content/${prj}/**/*.*`).forEach(file => {
     const fileKey = file.replace(`src/content/${prj}/`, '')
     obj[fileKey] = file.endsWith('.md')
-      ? requireMarkdown(path.resolve(file))
+      ? parseMarkdown(file)
       : fs.readFileSync(file, 'utf8')
   })
   return mapImports(obj)
@@ -22,46 +24,36 @@ const fetchContent = prj => {
 
 const envConfig = {}
 
-if (process.env.PULL_REQUEST) {
-  envConfig.siteRoot = (
-    process.env.PULL_REQUEST === 'true'
-      ? process.env.DEPLOY_PRIME_URL
-      : process.env.URL
-  ).replace('http://', 'https://')
-}
-
 const staticProps = {
-  footer: requireMarkdown(path.resolve('src/content/footer.md')),
+  footer: parseMarkdown('src/content/footer.md'),
   index: {
-    content: requireMarkdown(
-      path.resolve(path.resolve('src/content/readme.md'))
-    ),
+    content: parseMarkdown('src/content/readme.md'),
     title: 'You Might Not Need *',
   },
   lodash: {
-    content: requireMarkdown(path.resolve('./src/content/lodash.md')),
+    content: parseMarkdown('./src/content/lodash.md'),
     title: 'You Might Not Need Lodash',
     data: fetchContent('lodash'),
   },
   'lodash/missing': {
-    content: requireMarkdown(path.resolve('src/content/lodash-missing.md')),
+    content: parseMarkdown('src/content/lodash-missing.md'),
     title: 'You Might Not Need Lodash',
   },
   'date-fns': {
-    content: requireMarkdown(path.resolve('./src/content/date-fns.md')),
+    content: parseMarkdown('./src/content/date-fns.md'),
     title: 'You Might Not Need date-fns',
     data: fetchContent('date-fns'),
   },
   'date-fns/missing': {
-    content: requireMarkdown(path.resolve('src/content/date-fns-missing.md')),
+    content: parseMarkdown('src/content/date-fns-missing.md'),
     title: 'You Might Not Need date-fns',
   },
   momentjs: {
-    content: requireMarkdown(path.resolve('src/content/moment.md')),
+    content: parseMarkdown('src/content/moment.md'),
     title: 'You Might Not Need Moment.js',
   },
   'css': {
-    content: requireMarkdown(path.resolve('src/content/css.md')),
+    content: parseMarkdown('src/content/css.md'),
     title: 'You Might Not Need Css',
   },
 }
