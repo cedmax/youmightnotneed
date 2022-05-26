176d1ff31a70edd94cac35c10286c585b39f8868
Roman Gusev <Roman_Gusev1@epam.com>
2022-05-26 17:38:28 +0200

    Add test for variadic xor

--- src/content/lodash/array/xor/vanilla.js
+++ src/content/lodash/array/xor/vanilla.js
@@ -10,3 +10,6 @@ module.exports.unique = xor([2, 1, 4], [2, 3, 2, 6])
 
 module.exports.duplicates = xor([1, 1, 2], [2, 2, 3])
 // => [1, 3]
+
+module.exports.variadic = xor([1], [2], [1, 3], [2, 4])
+// => [3, 4]
