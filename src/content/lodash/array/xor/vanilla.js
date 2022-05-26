9165d7ba7b68dd66413e7e7ebe3f5de6e9d6e88b
Roman Gusev <Roman_Gusev1@epam.com>
2022-05-26 17:47:44 +0200

    Extract binaryXor from xor

--- src/content/lodash/array/xor/vanilla.js
+++ src/content/lodash/array/xor/vanilla.js
@@ -1,14 +1,13 @@
-const xor = (...arrays) => {
-  // this function can be used as a standalone xor if you only ever need it to work with two arguments
-  const binaryXor = (arr1, arr2) => {
-    const a = arr1.filter(x => !arr2.includes(x))
-    const b = arr2.filter(x => !arr1.includes(x))
+// this function can be used as a standalone xor if you only
+// ever need it to work with two arguments
+const binaryXor = (arr1, arr2) => {
+  const a = arr1.filter(x => !arr2.includes(x))
+  const b = arr2.filter(x => !arr1.includes(x))
 
-    return Array.from(new Set([...a, ...b]))
-  }
-
-  return arrays.reduce(binaryXor, [])
+  return Array.from(new Set([...a, ...b]))
 }
+
+const xor = (...arrays) => arrays.reduce(binaryXor, [])
 
 module.exports.unique = xor([2, 1, 4], [2, 3, 2, 6])
 // => [1, 4, 3, 6]
