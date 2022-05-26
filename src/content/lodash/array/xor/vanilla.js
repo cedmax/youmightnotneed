7fd839a464d826c085b6d2e3cdb4bd83e946306b
Roman Gusev <Roman_Gusev1@epam.com>
2022-05-26 17:20:03 +0200

    Fix xor implementation to return unique values

--- src/content/lodash/array/xor/vanilla.js
+++ src/content/lodash/array/xor/vanilla.js
@@ -2,7 +2,7 @@ const xor = (arr1, arr2) => {
   const a = arr1.filter(x => !arr2.includes(x))
   const b = arr2.filter(x => !arr1.includes(x))
 
-  return [...a, ...b]
+  return Array.from(new Set([...a, ...b]))
 }
 
 module.exports.unique = xor([2, 1, 4], [2, 3, 2, 6])
