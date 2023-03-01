e064808839ca1b3691d4fcb44a878076844d0513
Roman Gusev <Roman_Gusev1@epam.com>
2023-03-01 14:09:00 +0100

    Split comment wording update

--- src/content/lodash/string/split/vanilla.js
+++ src/content/lodash/string/split/vanilla.js
@@ -7,14 +7,15 @@ exports.justTwo = 'a-b-c'.split('-').slice(0, 2)
 // When the split is used with the empty separator, `String#split` and `_.split`
 // will have the different behavior:
 //
-// _.split takes user-perceived characters (grapheme clusters) and unicode
-// characters (codepoints) into account;
+// `_.split` with empty string as a separator takes user-perceived characters
+// (grapheme clusters) and unicode characters (codepoints) into account;
 //
-// `String#split` does not take them into account and will return string split
-// by UTF-16 codeunits, which will destroy the surrogate pairs.
+// `String#split` with empty string as a separator does not take them into
+// account and will return string split by UTF-16 codeunits, which will destroy
+// the surrogate pairs.
 //
-// In order to avoid this problem, `Intl.Segmenter` API could be used
-exports.graphemes = [...new Intl.Segmenter().segment('foo👨‍👨‍👧‍👦')].map(
+// To avoid this issue, `Intl.Segmenter` API could be used
+exports.graphemes = Array.from(new Intl.Segmenter().segment('foo👨‍👨‍👧‍👦')).map(
   ({ segment }) => segment
 )
 // => ['f', 'o', 'o', '👨‍👨‍👧‍👦']
