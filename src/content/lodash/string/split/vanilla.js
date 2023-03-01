e0446dafd35974def8f211d94300888bf54c4282
Roman Gusev <rgusev2@gmail.com>
2023-03-01 12:01:52 +0100

    Add comment regarding Intl.Segmenter

--- src/content/lodash/string/split/vanilla.js
+++ src/content/lodash/string/split/vanilla.js
@@ -4,6 +4,16 @@ exports.all = 'a-b-c'.split('-')
 exports.justTwo = 'a-b-c'.split('-').slice(0, 2)
 // => ['a', 'b']
 
+// When the split is used with the empty separator, `String#split` and `_.split`
+// will have the different behavior:
+//
+// _.split takes user-perceived characters (grapheme clusters) and unicode
+// characters (codepoints) into account;
+//
+// `String#split` does not take them into account and will return string split
+// by UTF-16 codeunits, which will destroy the surrogate pairs.
+//
+// In order to avoid this problem, `Intl.Segmenter` API could be used
 exports.graphemes = [...new Intl.Segmenter().segment('foo👨‍👨‍👧‍👦')].map(
   ({ segment }) => segment
 )
