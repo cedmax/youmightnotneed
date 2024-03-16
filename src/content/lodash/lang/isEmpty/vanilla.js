c454485ff47ec712602beb2e508855f6cbfc01e8
Marco Cedaro <cedmax@users.noreply.github.com>
2024-03-16 00:17:59 +0000

    isEmpty

---
+++ src/content/lodash/lang/isEmpty/vanilla.js
@@ -0,0 +1,40 @@
+const isEmpty = obj =>
+  [Object, Array].includes((obj || {}).constructor)
+    ? !Object.entries(obj || {}).length
+    : !obj.length && !obj.size
+
+exports.null = isEmpty(null)
+// => true
+
+exports.boolean = isEmpty(true)
+// => true
+
+exports.number = isEmpty(1)
+// => true
+
+exports.string = isEmpty('not empty')
+// => false
+
+exports.set = isEmpty(new Set([1, 2, 3]))
+// => false
+
+exports.map = isEmpty(new Map([['a', 1]]))
+// => false
+
+exports.array = isEmpty([1, 2, 3])
+// => false
+
+exports.object = isEmpty({ 'a': 1 })
+// => false
+
+exports.emptyArray = isEmpty([])
+// => true
+
+exports.emptyString = isEmpty('')
+// => true
+
+exports.emptySet = isEmpty(new Set())
+// => true
+
+exports.emptyMap = isEmpty(new Map())
+// => true
