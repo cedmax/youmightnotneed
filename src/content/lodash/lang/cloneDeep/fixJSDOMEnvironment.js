b2128517b4109f96e5e13da792bbac8988dfbb37
Marco Cedaro <cedmax@users.noreply.github.com>
2024-03-16 00:31:47 +0000

    fix coverage

--- src/content/lodash/lang/cloneDeep/fixJSDOMEnvironment.js
+++ src/content/lodash/lang/cloneDeep/fixJSDOMEnvironment.js
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import JSDOMEnvironment from 'jest-environment-jsdom'
 
 // https://github.com/facebook/jest/blob/v29.4.3/website/versioned_docs/version-29.4/Configuration.md#testenvironment-string
